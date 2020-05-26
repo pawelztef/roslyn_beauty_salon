52e183beab0cbff9628d7e553d49f5e7c2b5ad9e
Pavel <pawelztef@gmail.com>
2020-05-26 22:37:07 +0100

    Slider styles and component.

--- frontend/src/components/Slider.js
+++ frontend/src/components/Slider.js
@@ -3,11 +3,20 @@ import React from 'react'
 const Slider = (props) =>  {
   const {data} = props
   console.log('Slider.render data', data)
-  const url = data.item[0].image.url
-  console.log(url)
+  const item = data.item[0]
+  const {image} = item
+  console.log("Slider.render item", item)
+  console.log("Slider.render image", image)
   return (
    <div className="pz-animated-banner">
-    <img src={"http://localhost:1337" + url} alt=""/>
+    <img className="pz-animated-banner__item" 
+         src={image.url} 
+         alt={image.alternativeText}/>
+    <div className="pz-animated-banner__item-top-layer">
+      <div className="pz-animated-banner__item-title">{item.title}</div>
+      <div className="pz-animated-banner__item-subtitle">{item.subtitle}</div>
+      <p className="pz-btn">View Our Services</p>
+    </div>
    </div>
   )
 }
