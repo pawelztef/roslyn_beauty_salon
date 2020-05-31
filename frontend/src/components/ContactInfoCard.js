af146d276294255b03f3506f96db18543946fe07
Pavel <pawelztef@gmail.com>
2020-05-31 17:55:21 +0100

    Contact info component created.

---
+++ frontend/src/components/ContactInfoCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ContactInfoCard = (props) => {
+  return(
+    <div className="pz-container-wrapper contact-info">
+             
+    </div>  
+  )
+}
+
+export default ContactInfoCard
