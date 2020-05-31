35286fe075fd3f5e5db2d2c9c536e18be746f21d
Pavel <pawelztef@gmail.com>
2020-05-31 13:28:22 +0100

    Classes renamed for future css framework.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import mockPage from './utils/page'
 import CardGroup from './components/CardGroup'
 import Slider from './components/Slider'
 import Banner from './components/Banner'
+import Welcome from './components/Welcome'
 import Layout from './components/Layout'
 
 function App() {
@@ -13,10 +14,11 @@ function App() {
     <>
       <Layout>
           <Slider data={mockPage.content[0]}/> 
-          <div class="container-wrapper">
+          <div class="pz-container-wrapper">
              <CardGroup data={mockPage.content[1]} />
              <Banner data={mockPage.content[2]} />
           </div>
+             <Welcome data={[mockPage.content[3], mockPage.content[4]]} />
       </Layout>
     </>
   )
