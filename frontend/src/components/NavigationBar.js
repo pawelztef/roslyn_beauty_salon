0562199e76c186ac8d813cbb19a3ab427ae8a23c
Pavel <pawelztef@gmail.com>
2020-06-05 02:04:17 +0100

    NavigationBar component added.

---
+++ frontend/src/components/NavigationBar.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import styled from 'styled-components'
+
+  const Header = styled.header`
+    background: white;
+    
+  `
+
+  const TopNav = styled.nav`
+    font-size: .9rem;
+    padding-top: 0.5em;
+    padding-bottom: 0.5em;
+  `
+  const BottomNav = styled.nav`
+    padding-bottom: 1.8em;
+    padding-top: 1.8em;
+  `
+
+  const Brand = styled.a`
+  `
+
+  const BrandImage = styled.img`
+    max-height: 46px;
+  `
+  
+  const ContactDetials = styled.div`
+
+  `
+
+export default function NavigationBar() {
+
+  return(
+
+    <Header className="pz-nav">
+        <TopNav className="pz-nav__top-row">
+          <div className="pz-container-wrapper">
+            hello
+          </div>
+        </TopNav>
+        <BottomNav className="pz-nav__bottom-row">
+          <div className="pz-container-wrapper">
+
+            <Brand className="pz-nav__brand"
+                   href="https://pawelztef.me"
+                   target="_blank">
+              <BrandImage className="pz-nav__brand-img" 
+                   src="http://muji.ancorathemes.com/wp-content/uploads/2017/12/logo_retina.png"/>
+            </Brand>
+
+          </div>
+        </BottomNav>
+    </Header>
+
+  )
+}
+
+
