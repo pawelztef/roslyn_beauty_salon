50bfebc20f89a3704a522332b739be175412864b
Pavel <pawelztef@gmail.com>
2020-06-05 13:50:54 +0100

    Logo extracted to component.

--- frontend/src/components/BottomNav.js
+++ frontend/src/components/BottomNav.js
@@ -1,27 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
+import Brand from './Brand'
 
 const Nav = styled.nav`
   padding-bottom: 1.8em;
   padding-top: 1.8em;
 `
-const Brand = styled.a`
-`
-const BrandImage = styled.img`
-  max-height: 46px;
-`
-
 export default function BottomNav() {
   return(
     <Nav className="pz-nav__bottom-row">
       <div className="pz-container-wrapper">
 
-        <Brand className="pz-nav__brand"
-          href="https://pawelztef.me"
-          target="_blank">
-          <BrandImage className="pz-nav__brand-img" 
-            src="http://muji.ancorathemes.com/wp-content/uploads/2017/12/logo_retina.png"/>
-        </Brand>
+        <Brand />
 
       </div>
     </Nav>
---
+++ frontend/src/components/Brand.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const BrandHref = styled.a`
+`
+const BrandImage = styled.img`
+  max-height: 46px;
+`
+
+export default function Brand() {
+  return(
+  <BrandHref className="pz-nav__brand"
+    href="https://pawelztef.me"
+    target="_blank">
+    <BrandImage className="pz-nav__brand-img" 
+      src="http://muji.ancorathemes.com/wp-content/uploads/2017/12/logo_retina.png"/>
+  </BrandHref>
+  )
+}
