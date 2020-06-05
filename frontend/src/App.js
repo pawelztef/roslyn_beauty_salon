bceff1115266cab2a5829dd13ae7ddd4026f5059
Pavel <pawelztef@gmail.com>
2020-06-05 02:02:08 +0100

    Main app file refactored.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,20 +6,38 @@ import CardGroup from './components/CardGroup'
 import Slider from './components/Slider'
 import Banner from './components/Banner'
 import Welcome from './components/Welcome'
-import Layout from './components/Layout'
+import EmptySpace from './components/EmptySpace'
+import NavigationBar from './components/NavigationBar'
 
 function App() {
   console.log('App.render mockPage', mockPage)
   return (
     <>
-      <Layout>
-          <Slider data={mockPage.content[0]}/> 
-          <div class="pz-container-wrapper">
-             <CardGroup data={mockPage.content[1]} />
-             <Banner data={mockPage.content[2]} />
+      <div className="pz-body-wrapper">
+        <div className="responsivnes-indicator"></div>
+          <div className="pz-page-wrapper">
+
+            <NavigationBar />
+
+            <div className="pz-page-container-wrapper">
+
+            <Slider data={mockPage.content[0]}/> 
+
+              <div class="pz-container-wrapper">
+
+                <CardGroup data={mockPage.content[1]} />
+
+                <Banner data={mockPage.content[2]} />
+
+              </div>
+
+            <Welcome data={[mockPage.content[3], mockPage.content[4]]} />
+
+            </div>
           </div>
-             <Welcome data={[mockPage.content[3], mockPage.content[4]]} />
-      </Layout>
+
+        <EmptySpace />
+      </div>
     </>
   )
 }
