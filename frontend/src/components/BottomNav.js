37423ff2030dc44a094167b2d93d6fa3d29357d6
Pavel <pawelztef@gmail.com>
2020-06-05 18:46:43 +0100

    Sticky navbar.

--- frontend/src/components/BottomNav.js
+++ frontend/src/components/BottomNav.js
@@ -6,14 +6,32 @@ const Nav = styled.nav`
   padding-bottom: 1.8em;
   padding-top: 1.8em;
 `
-export default function BottomNav() {
-  return(
-    <Nav className="pz-nav__bottom-row">
-      <div className="pz-container-wrapper">
 
-        <Brand />
+class BottomNav extends React.Component {
 
-      </div>
-    </Nav>
-  )
+  componentDidMount() {
+    const nav = document.querySelector('.pz-nav__bottom-row')
+    const topOfBottomNav = nav.offsetTop
+    window.addEventListener('scroll', () => {
+      if(window.scrollY >= topOfBottomNav) {
+        nav.classList.add('pz-nav__fixed')
+        document.body.style.paddingTop = (nav.offsetHeight - 5) + "px"
+      } else {
+        nav.classList.remove('pz-nav__fixed')
+        document.body.style.paddingTop = 0
+      }
+    })
+  }
+
+  render(){
+    return(
+      <Nav className="pz-nav__bottom-row">
+        <div className="pz-container-wrapper">
+          <Brand />
+        </div>
+      </Nav>
+    )
+  }  
 }
+
+export default BottomNav
