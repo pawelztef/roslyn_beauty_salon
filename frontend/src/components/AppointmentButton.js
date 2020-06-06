0e3c798e124efd1519a467fbdc64c04132e82e12
Pavel <pawelztef@gmail.com>
2020-06-06 13:32:05 +0100

    AppointmentButton padding fixed.

---
+++ frontend/src/components/AppointmentButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+
+export default function AppointmentButton() {
+  return(
+    <p className="pz-btn pz-nav__appointment-button">Make appointment</p>
+  )
+}
