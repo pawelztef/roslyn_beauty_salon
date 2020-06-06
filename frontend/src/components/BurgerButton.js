0a0a892297132077e23fc4ed372baf3cad5d2441
Pavel <pawelztef@gmail.com>
2020-06-06 16:05:01 +0100

    BurgerButton component created.

---
+++ frontend/src/components/BurgerButton.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+import { RiMenuLine } from "react-icons/ri"
+
+const BurrgerWrp = styled.span`
+  display: none;
+  position: relative;
+  -webkit-border-radius: 50%;
+  -ms-border-radius: 50%;
+  border-radius: 50%;
+  height: 40px;
+  width: 40px;
+  top: 4px;
+  text-align: center;
+  line-height: 40px;
+  svg {
+    font-size: 18px;
+  }
+`
+
+export default function BurgerButton() {
+  return(
+    <BurrgerWrp className='pz-nav__burger-button pz-visible-md'>
+      <RiMenuLine />
+    </BurrgerWrp>
+  )
+}
