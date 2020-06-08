2ecede1f11eef614b5be9957eafabe32cfc440df
Pavel <pawelztef@gmail.com>
2020-06-08 22:32:37 +0100

    SocialList - styled components removed.

--- frontend/src/components/SocialList.js
+++ frontend/src/components/SocialList.js
@@ -3,29 +3,19 @@ import styled from 'styled-components'
 import { RiFacebookCircleLine, RiTwitterLine, RiInstagramLine, RiPinterestLine } from "react-icons/ri"
 
 const List = styled.ul`
-  list-style: none;
-  display: flex;
-  padding: 0;
-  margin: 0;
-  border-left: 1px black solid;
-  margin-left: 20px;
-  padding-left: 5px;
 `
 const ListItem = styled.li`
-  margin-left: 10px;
-  padding: 0px 10px;
-  border-radius: 56%;
 `
 
 export default function SocialList() {
   return(
 
-  <List className="pz-social-list">
-    <ListItem className="pz-social-list__item"> <RiFacebookCircleLine /> </ListItem>
-    <ListItem className="pz-social-list__item"> <RiTwitterLine /> </ListItem>
-    <ListItem className="pz-social-list__item"> <RiInstagramLine /> </ListItem>
-    <ListItem className="pz-social-list__item"> <RiPinterestLine /> </ListItem>
-  </List>
+  <ul className="pz-social-list">
+    <li className="pz-social-list__item"> <RiFacebookCircleLine /> </li>
+    <li className="pz-social-list__item"> <RiTwitterLine /> </li>
+    <li className="pz-social-list__item"> <RiInstagramLine /> </li>
+    <li className="pz-social-list__item"> <RiPinterestLine /> </li>
+  </ul>
 
 
   )
