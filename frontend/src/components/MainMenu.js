8b8b4e4c789e1c1bc58816011308cd4beaf97add
Pavel <pawelztef@gmail.com>
2020-06-08 22:32:59 +0100

    MainMenu - styled components removed.

--- frontend/src/components/MainMenu.js
+++ frontend/src/components/MainMenu.js
@@ -1,45 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
-import variables from '../assets/sass/1-base/_variables.sass'
 
-const MenuList = styled.ul`
-  list-style: none;
-  flex: 2;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  align-items: center;
-  padding: 0;
-  margin: 0 2rem;
-
-`
-
-const MenuItem = styled.li`
-  text-decoration: none;
-  text-transform: uppercase;
-  font-weight: bold;
-  list-style: 0;
-  border-radius: 30px;
-`
 
 export default function MainMenu() {
   return(
-    <MenuList className="pz-menu pz-invisible-md">
-      <MenuItem className="pz-menu__item pz-menu__item-active"> 
+    <ul className="pz-menu">
+      <li className="pz-menu__item pz-menu__item-active"> 
         <a href="htts://pawelztef.me">Services</a>
-      </MenuItem>
-      <MenuItem className="pz-menu__item"> 
+      </li>
+      <li className="pz-menu__item"> 
         <a href="htts://pawelztef.me">About</a>
-      </MenuItem>
-      <MenuItem className="pz-menu__item"> 
-        <a href="htts://pawelztef.me">Team</a>
-      </MenuItem>
-      <MenuItem className="pz-menu__item"> 
+      </li>
+      <li className="pz-menu__item"> <a href="htts://pawelztef.me">Team</a>
+      </li>
+      <li className="pz-menu__item"> 
         <a href="htts://pawelztef.me">Contact</a>
-      </MenuItem>
-      <MenuItem className="pz-menu__item">
+      </li>
+      <li className="pz-menu__item">
         <a href="htts://pawelztef.me">Blog</a>
-      </MenuItem>
-    </MenuList>
+      </li>
+    </ul>
   )
 }
