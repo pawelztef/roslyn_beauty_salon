7324746ce86769eebeae6bef85d34219f842e313
Pavel <pawelztef@gmail.com>
2020-06-10 16:28:54 +0100

    Newsletter component created.

---
+++ frontend/src/components/Newsletter.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const newsletter = () => (
+  <div className="pz-newsletter__row">
+    <div class="pz-container-wrapper">
+      <div className="pz-newsletter__container">
+        <div className="pz-newsletter__item">
+          <span>Stay tuned with updates</span>
+        </div>
+        <div className="pz-newsletter__item">
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default newsletter
