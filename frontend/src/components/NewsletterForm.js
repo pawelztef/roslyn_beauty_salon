3c7965f3fc224a231bf777036770f7048d05f55e
Pavel <pawelztef@gmail.com>
2020-06-12 16:24:48 +0100

    Autocomplete off on email entry.

--- frontend/src/components/NewsletterForm.js
+++ frontend/src/components/NewsletterForm.js
@@ -1,8 +1,20 @@
-import React form 'react'
+import React from 'react'
 
-cont newsletterForm = () => (
-
-
+const newsletterForm = () => (
+  <form className="pz-newsletter-form" action="">
+    <div className="pz-newsletter-form-row-1">
+      <input id="pz-newsletter-email-input" 
+        type="email" 
+        placeholder="enter your email"
+        autocomplete="off"/>
+      <input id="pz-newsletter-button" type="submit" value="submit" />
+    </div>
+    <div className="pz-newsletter-form-row-2">
+      <input id="pz-newsletter-agrement" type="checkbox" name="newsletter-agrement" />
+      <label for="pz-newsletter-agrement">I have read and agree to the temrs & conditions.</label>
+      <label for="pz-newsletter-agrement">See our <a href="https://pawelztef.me">Privacy Policy</a>.</label>
+    </div>
+  </form>
 )
 
 export default newsletterForm
