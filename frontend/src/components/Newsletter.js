a0926c94c6eb8a2739a386af38e32814690d3bbb
Pavel <pawelztef@gmail.com>
2020-06-12 16:25:08 +0100

    Call to newsletter form fomr newsletter component.

--- frontend/src/components/Newsletter.js
+++ frontend/src/components/Newsletter.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import NewsletterForm from './NewsletterForm'
 
 const newsletter = () => (
   <div className="pz-newsletter__row">
     <div class="pz-container-wrapper">
       <div className="pz-newsletter__container">
         <div className="pz-newsletter__item">
-          <span>Stay tuned with updates</span>
+          <h4>Stay tuned with updates</h4>
         </div>
         <div className="pz-newsletter__item">
+          <NewsletterForm />
         </div>
       </div>
     </div>
