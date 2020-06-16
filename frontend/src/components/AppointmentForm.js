437538c70ca5adb8065331bbaa943685970b2aaa
Pavel <pawelztef@gmail.com>
2020-06-16 07:24:44 +0100

    Comments to appintment form.

--- frontend/src/components/AppointmentForm.js
+++ frontend/src/components/AppointmentForm.js
@@ -5,10 +5,10 @@ const appointmentForm = () => {
     <div className="pz-appointment__form-wrapper">
       <form className="pz-appointment__form" >
 
-{/* Row 1 Start */}
+        {/* Row 1 Start */}
         <div className="pz-appointment__form-row pz-appointment__row-1">
 
-{/* Section 1 Start */}
+          {/* Section 1 Start */}
           <div className="pz-appointment__form-section">
 
             <div className="pz-appointment__form-row ">
@@ -56,33 +56,37 @@ const appointmentForm = () => {
             </div>
 
           </div>
-{/* Section 1 End*/}
+          {/* Section 1 End*/}
 
-{/* Section 2 Start */}
+          {/* Section 2 Start */}
           <div className="pz-appointment__form-section">
             <div className="pz-appointment__form-input">
               <textarea id="appointment-message" 
                 name="appointment-message" 
-                rows="11" 
-                cols="40"
                 placeholder="Your Message"
               >
               </textarea>
             </div>
           </div>
-{/* Section 2 End */}
+          {/* Section 2 End */}
 
         </div>
-{/* Row 1 End */}
+        {/* Row 1 End */}
 
-{/* Row 2 Start */}
-        <div className="pz-appointment__form-row">
-          <div className="pz-appintment__form-section">
+        {/* Row 2 Start */}
+        <div className="pz-appointment__form-row pz-appointment__row-2">
+          <div className="pz-appointment__form-section">
+            <label for="pz-appointment-agrement">
+              <input id="pz-appointment-agrement" type="checkbox" name="appointment-agrement" />
+              I have read and agree to the temrs & conditions.
+            </label>
+            <label for="pz-appointment-agrement">See our <a href="https://pawelztef.me">Privacy Policy</a>.</label>
           </div>
-          <div className="pz-appintment__form-section">
+          <div className="pz-appointment__form-section">
+            <button className="pz-btn">Submit</button>
           </div>
         </div>
-{/* Row 2 End */}
+        {/* Row 2 End */}
 
       </form>
     </div>
