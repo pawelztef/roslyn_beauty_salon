5935abcec70122e92dda74a84b4bffd6c5548a99
Pavel <pawelztef@gmail.com>
2020-06-20 08:44:08 +0100

    Social links on team thumbs styled.

--- frontend/src/components/TeamThumbs.js
+++ frontend/src/components/TeamThumbs.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { RiFacebookCircleLine, RiTwitterLine, RiInstagramLine, RiPinterestLine } from "react-icons/ri"
 
 const profileThumb = (props) => {
   return(
     <div className="pz-team">
       <div className="pz-team__profile">
-        <img src="http://muji.ancorathemes.com/wp-content/uploads/2017/12/team-11-copyright-450x450.jpg" alt="" />
+        <div className="pz-profile__image-wrapper">
+          <div className="pz-profile__mask">
+          </div>
+          <img src="http://muji.ancorathemes.com/wp-content/uploads/2017/12/team-11-copyright-450x450.jpg" alt="" />
+          <div className="pz-profile__social">
+            <ul className="pz-social-list">
+              <li className="pz-social-list__item"> <RiFacebookCircleLine /> </li>
+              <li className="pz-social-list__item"> <RiInstagramLine /> </li>
+              <li className="pz-social-list__item"> <RiPinterestLine /> </li>
+            </ul>
+          </div>
+        </div>
         <div className="pz-team__profile-info">
           <div className="pz-profile__header">
             <div className="pz-profile__header-title">Kylie Porter</div>
@@ -12,7 +24,18 @@ const profileThumb = (props) => {
         </div>
       </div>
       <div className="pz-team__profile">
-        <img src="http://muji.ancorathemes.com/wp-content/uploads/2017/12/team-11-copyright-450x450.jpg" alt="" />
+        <div className="pz-profile__image-wrapper">
+          <div className="pz-profile__mask">
+          </div>
+          <img src="http://muji.ancorathemes.com/wp-content/uploads/2017/12/team-11-copyright-450x450.jpg" alt="" />
+          <div className="pz-profile__social">
+            <ul className="pz-social-list">
+              <li className="pz-social-list__item"> <RiFacebookCircleLine /> </li>
+              <li className="pz-social-list__item"> <RiInstagramLine /> </li>
+              <li className="pz-social-list__item"> <RiPinterestLine /> </li>
+            </ul>
+          </div>
+        </div>
         <div className="pz-team__profile-info">
           <div className="pz-profile__header">
             <div className="pz-profile__header-title">Kylie Porter</div>
@@ -20,7 +43,18 @@ const profileThumb = (props) => {
         </div>
       </div>
       <div className="pz-team__profile">
-        <img src="http://muji.ancorathemes.com/wp-content/uploads/2017/12/team-11-copyright-450x450.jpg" alt="" />
+        <div className="pz-profile__image-wrapper">
+          <div className="pz-profile__mask">
+          </div>
+          <img src="http://muji.ancorathemes.com/wp-content/uploads/2017/12/team-11-copyright-450x450.jpg" alt="" />
+          <div className="pz-profile__social">
+            <ul className="pz-social-list">
+              <li className="pz-social-list__item"> <RiFacebookCircleLine /> </li>
+              <li className="pz-social-list__item"> <RiInstagramLine /> </li>
+              <li className="pz-social-list__item"> <RiPinterestLine /> </li>
+            </ul>
+          </div>
+        </div>
         <div className="pz-team__profile-info">
           <div className="pz-profile__header">
             <div className="pz-profile__header-title">Kylie Porter</div>
@@ -28,7 +62,18 @@ const profileThumb = (props) => {
         </div>
       </div>
       <div className="pz-team__profile">
-        <img src="http://muji.ancorathemes.com/wp-content/uploads/2017/12/team-11-copyright-450x450.jpg" alt="" />
+        <div className="pz-profile__image-wrapper">
+          <div className="pz-profile__mask">
+          </div>
+          <img src="http://muji.ancorathemes.com/wp-content/uploads/2017/12/team-11-copyright-450x450.jpg" alt="" />
+          <div className="pz-profile__social">
+            <ul className="pz-social-list">
+              <li className="pz-social-list__item"> <RiFacebookCircleLine /> </li>
+              <li className="pz-social-list__item"> <RiInstagramLine /> </li>
+              <li className="pz-social-list__item"> <RiPinterestLine /> </li>
+            </ul>
+          </div>
+        </div>
         <div className="pz-team__profile-info">
           <div className="pz-profile__header">
             <div className="pz-profile__header-title">Kylie Porter</div>
