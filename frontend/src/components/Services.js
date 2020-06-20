da064a2b2eb942c5dc1867089ebf7917798b3198
Pavel <pawelztef@gmail.com>
2020-06-20 09:59:39 +0100

    Call to servicesList from services.

--- frontend/src/components/Services.js
+++ frontend/src/components/Services.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import EmptySpace from './EmptySpace'
+import ServicesList from './ServicesList'
 
 const services = props => {
   return(
@@ -9,6 +10,11 @@ const services = props => {
         <div className="pz-services__container">
           <h2 className="pz-section__header">Choose the service you need</h2>
         </div>
+      </div>
+      <ServicesList />
+      <EmptySpace />
+      <div className="pz-section__content">
+        <button className="pz-btn">View all Services</button>
       </div>
       <EmptySpace />
     </div>
