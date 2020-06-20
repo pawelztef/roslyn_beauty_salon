1b969a07909edfedc7c9e3ee8f0f0f88b5eb544f
Pavel <pawelztef@gmail.com>
2020-06-20 09:54:05 +0100

    Headers and containers on services component.

--- frontend/src/components/Services.js
+++ frontend/src/components/Services.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import EmptySpace from './EmptySpace'
 
 const services = props => {
   return(
     <div className="pz-services__wrapper">
+      <EmptySpace />
       <div class="pz-container-wrapper">
-        <div>hello</div>
+        <div className="pz-services__container">
+          <h2 className="pz-section__header">Choose the service you need</h2>
+        </div>
       </div>
+      <EmptySpace />
     </div>
   )
 }
