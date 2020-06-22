5c5ad74448cdb190e658548ba5f6a4d052f6ce5e
Pavel <pawelztef@gmail.com>
2020-06-22 09:17:34 +0100

    GalleryPage component created.

---
+++ frontend/src/components/GalleryPage.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import mockPage from '../utils/page'
+
+import CardGroup from './CardGroup'
+import Slider from './Slider'
+import Banner from './Banner'
+import Welcome from './Welcome'
+import EmptySpace from './EmptySpace'
+import Newsletter from './Newsletter'
+import Appointment from './Appointment'
+import Team from './Team'
+import Services from './Services'
+import Testimony from './Testimony'
+import Portfolio from './Portfolio'
+
+class GalleryPage  extends React.Component {
+  render() {
+    return(
+      <div className="pz-page-container-wrapper">
+
+        Gallery Page
+        
+
+      </div>
+
+    )
+  }
+}
+
+export default GalleryPage 
