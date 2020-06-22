3ad506282cfd2100d17fc014291075539cd74c76
Pavel <pawelztef@gmail.com>
2020-06-22 08:26:00 +0100

    Home page extracted from App.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,10 @@
 import React from 'react'
 import './assets/sass/main.sass'
-import mockPage from './utils/page'
 
-import CardGroup from './components/CardGroup'
-import Slider from './components/Slider'
-import Banner from './components/Banner'
-import Welcome from './components/Welcome'
-import EmptySpace from './components/EmptySpace'
 import NavigationBar from './components/NavigationBar'
 import MobileNav from './components/MobileNav'
 import Footer from './components/Footer'
-import Newsletter from './components/Newsletter'
-import Appointment from './components/Appointment'
-import Team from './components/Team'
-import Services from './components/Services'
-import Testimony from './components/Testimony'
-import Portfolio from './components/Portfolio'
+import Home from './components/Home'
 
 class App extends React.Component {
   state = {
@@ -35,37 +24,13 @@ class App extends React.Component {
         <div className="pz-body-wrapper">
           <div className="responsivnes-indicator"></div>
           <div className="pz-page-wrapper">
-
             <NavigationBar mobileMenuToggleHandler={this.mobileMenuToggleHandler} />
             <MobileNav show={this.state.mobileMenuOpen}
               click={this.mobileMenuToggleHandler} />
 
-            <div className="pz-page-container-wrapper">
+            <Home />
 
-              <Slider data={mockPage.content[0]}/> 
-
-
-              <CardGroup data={mockPage.content[1]} />
-
-              <Banner data={mockPage.content[2]} />
-
-              <Welcome data={[mockPage.content[3], mockPage.content[4]]} />
-
-              <Testimony/>
-
-              <Services />
-
-              <Portfolio />
-
-              <Team />
-
-              <Appointment />
-
-              <Newsletter />
-
-              <Footer />
-
-            </div>
+            <Footer />
           </div>
         </div>
       </>
