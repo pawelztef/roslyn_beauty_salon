d9f4d3dd9fef916227406204c3060f98bf32beb7
Pavel <pawelztef@gmail.com>
2020-06-28 02:55:27 +0100

    Header component created.

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = (props) =>  {
+  return (
+   <div className="pz-header">
+     <img className="pz-header__background" src="/images/header-grad.png"/>
+     <h1 className="pz-header__title">About Us</h1>
+   </div>
+  )
+}
+
+
+export default Header
