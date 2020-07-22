e86a35ed01c963fc9d8a01c7cff789a5058eef86
Pavel <pawelztef@gmail.com>
2020-07-22 18:07:47 +0100

    Update to About page and its Header call.

--- frontend/src/components/AboutPage.js
+++ frontend/src/components/AboutPage.js
@@ -12,12 +12,16 @@ import Team from './Team'
 import Services from './Services'
 import OurStory from './OurStory'
 import Portfolio from './Portfolio'
+import bg from '../assets/images/header-grad.png'
 
 class AboutPage  extends React.Component {
   render() {
     return(
       <div className="pz-page-container-wrapper">
-        <Header title="About Us"/>
+        <Header 
+          title="About Us"
+          backgroundImage={bg}
+        />
         <EmptySpace />
         <Welcome data={[mockPage.content[3], mockPage.content[4]]} />
         <OurStory />
