760c695d45f3b68ee4cc00ffabe9fc132cb3b00f
Pavel <pawelztef@gmail.com>
2020-07-22 18:06:33 +0100

    Header modified. Image as

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = (props) =>  {
   return (
    <div className="pz-header">
-     <img className="pz-header__background" src="/images/header-grad.png"/>
+     <img className="pz-header__background" src={props.backgroundImage}/>
      <h1 className="pz-header__title">{props.title}</h1>
    </div>
   )
