7e948ad962eb6326bf386e81d304bf7c79de3a8d
Pavel <pawelztef@gmail.com>
2020-08-04 13:03:03 +0100

    Creating footer Api

---
+++ backend/api/footer/services/footer.js
@@ -0,0 +1,10 @@
+module.exports = {
+  /**
+   * Promise to fetch all records
+   *
+   * @return {Promise}
+   */
+  find(params, populate) {
+    return strapi.query('footer').find(params, ['menu', 'menu.pages' ])
+  },
+};
