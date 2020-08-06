0bf7fb4dfaa74f97668fd73c9517f7f054cac04c
Pavel <pawelztef@gmail.com>
2020-08-06 22:06:05 +0100

    Forms components apis & restructuring footer to use axios and feed it with strapi data.

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,45 +1,64 @@
 import React from 'react'
+import Axios from 'axios'
 import ContactDetails from './ContactDetails'
 import Brand from './Brand'
 import SocialList from './SocialList'
 
 
-const footer = () => (
-  <div className="pz-container-wrapper">
-    <footer className="pz-footer">
-      <div className="pz-footer__col-1">
-        <ContactDetails />
-      </div>
-      <div className="pz-footer__col-2">
-        <Brand />
-        <div className="pz-footer-meta">
-          <span>
-            <a href="https://pawelztef.me">Pawelztef</a>© 2020
-          </span>
-          <span>
-            All rights reserved
-          </span>
-        </div>
-        <SocialList />
-      </div>
-      <div className="pz-footer__col-3">
-        <div className="pz-footer__legal">
-          <span className="pz-footer__legal-item">
-            <a href="http://pawelztef.me">Privacy Policy</a>
-          </span>
-          <span className="pz-footer__legal-item">
-            <a href="http://pawelztef.me">Cookies Policy</a>
-          </span>
-          <span className="pz-footer__legal-item">
-            <a href="http://pawelztef.me">Terms And Conditions</a>
-          </span>
-          <span className="pz-footer__legal-item">
-            <a href="http://pawelztef.me">Refund Policy</a>
-          </span>
-        </div>
-      </div>
-    </footer>
-  </div>
-)
+export default class Footer extends React.Component {
 
-export default footer
+
+  state = {
+    menu: {},
+    social_media: [],
+    contact_details: {},
+    logo: {}
+  }
+
+  componentDidMount() {
+
+  }
+
+
+  render() {
+    return(
+      <div className="pz-container-wrapper">
+        <footer className="pz-footer">
+          <div className="pz-footer__col-1">
+            <ContactDetails />
+          </div>
+          <div className="pz-footer__col-2">
+            <Brand />
+            <div className="pz-footer-meta">
+              <span>
+                <a href="https://pawelztef.me">Pawelztef</a>© 2020
+              </span>
+              <span>
+                All rights reserved
+              </span>
+            </div>
+            <SocialList />
+          </div>
+          <div className="pz-footer__col-3">
+            <div className="pz-footer__legal">
+              <span className="pz-footer__legal-item">
+                <a href="http://pawelztef.me">Privacy Policy</a>
+              </span>
+              <span className="pz-footer__legal-item">
+                <a href="http://pawelztef.me">Cookies Policy</a>
+              </span>
+              <span className="pz-footer__legal-item">
+                <a href="http://pawelztef.me">Terms And Conditions</a>
+              </span>
+              <span className="pz-footer__legal-item">
+                <a href="http://pawelztef.me">Refund Policy</a>
+              </span>
+            </div>
+          </div>
+        </footer>
+      </div>
+    )
+  }
+}
+
+
