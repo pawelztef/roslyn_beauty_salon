b5f5a2b5551543f255452305b8e6f0ff3b041de7
Pavel <pawelztef@gmail.com>
2020-08-07 19:58:39 +0100

    Removing unnecessary imports.

--- frontend/src/components/AboutPage.js
+++ frontend/src/components/AboutPage.js
@@ -5,11 +5,8 @@ import Header from './Header'
 import Welcome from './Welcome'
 import EmptySpace from './EmptySpace'
 import Newsletter from './Newsletter'
-import Appointment from './Appointment'
 import Team from './Team'
-import Services from './Services'
 import OurStory from './OurStory'
-import Portfolio from './Portfolio'
 import bg from '../assets/images/header-grad.png'
 
 class AboutPage  extends React.Component {
