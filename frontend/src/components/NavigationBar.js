81f9a00d82283405303b4e37f6750aacc1ca854d
Pavel <pawelztef@gmail.com>
2020-08-07 19:48:43 +0100

    Removing unnecessary imports.

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import variables from '../assets/sass/1-base/_variables.sass'
 import styled from 'styled-components'
 import TopNav from './TopNav'
 import BottomNav from './BottomNav'
