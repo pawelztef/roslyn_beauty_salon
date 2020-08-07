91bb6f861418524306e6fb0e93dc97e6fd2a3e23
Pavel <pawelztef@gmail.com>
2020-08-07 19:54:40 +0100

    Removing unnecessary imports.

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -1,17 +1,14 @@
 import React from 'react'
-import styled from 'styled-components'
 import TopNav from './TopNav'
 import BottomNav from './BottomNav'
 
-const Header = styled.header`
-`
 
 export default function NavigationBar(props) {
   return(
-    <Header className="pz-nav">
+    <header className="pz-nav">
       <TopNav /> 
       <BottomNav mobileMenuToggleHandler={props.mobileMenuToggleHandler} /> 
-    </Header>
+    </header>
   )
 }
 
