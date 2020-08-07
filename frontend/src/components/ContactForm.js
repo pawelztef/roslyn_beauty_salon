0877c1b271a8ada83b1ee5f57f48c9d907e0014c
Pavel <pawelztef@gmail.com>
2020-08-07 19:36:53 +0100

    Removing unnecessary imports.

--- frontend/src/components/ContactForm.js
+++ frontend/src/components/ContactForm.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Button } from '@material-ui/core'
 import { MuiPickersUtilsProvider, DatePicker, TimePicker } from "@material-ui/pickers"
 import DateFnsUtils from '@date-io/date-fns'
 
