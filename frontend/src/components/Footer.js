d5ff67a693884e21ea07011edd42a82adae0906b
Pavel <pawelztef@gmail.com>
2020-08-07 18:32:48 +0100

    Passing data to footer menu.

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,6 +3,7 @@ import Axios from 'axios'
 import ContactDetails from './ContactDetails'
 import Brand from './Brand'
 import SocialList from './SocialList'
+import {Link} from 'react-router-dom'
 
 
 export default class Footer extends React.Component {
@@ -12,51 +13,55 @@ export default class Footer extends React.Component {
     menu: {},
     social_media: [],
     contact_details: {},
-    logo: {}
+    logo: {},
+    isLoading: true
   }
 
   async componentDidMount() {
     const response = await Axios.get('http://localhost:1337/footer')
     const { menu, social_media, contact_details, logo } = response.data[0]
-    this.setState({menu, social_media, contact_details, logo, isLoaded: true})
+    this.setState({menu, social_media, contact_details, logo, isLoading: false})
+    console.log("pages", this.state.menu.pages[0].id)
   }
 
 
   render() {
+    const { contact_details, menu, isLoading } = this.state
     return(
       <div className="pz-container-wrapper">
         <footer className="pz-footer">
-          <div className="pz-footer__col-1">
-            <ContactDetails {...this.state.contact_details} />
-          </div>
-          <div className="pz-footer__col-2">
-            <Brand />
-            <div className="pz-footer-meta">
-              <span>
-                <a href="https://pawelztef.me">Pawelztef</a>© 2020
-              </span>
-              <span>
-                All rights reserved
-              </span>
-            </div>
-            <SocialList />
-          </div>
-          <div className="pz-footer__col-3">
-            <div className="pz-footer__legal">
-              <span className="pz-footer__legal-item">
-                <a href="http://pawelztef.me">Privacy Policy</a>
-              </span>
-              <span className="pz-footer__legal-item">
-                <a href="http://pawelztef.me">Cookies Policy</a>
-              </span>
-              <span className="pz-footer__legal-item">
-                <a href="http://pawelztef.me">Terms And Conditions</a>
-              </span>
-              <span className="pz-footer__legal-item">
-                <a href="http://pawelztef.me">Refund Policy</a>
-              </span>
-            </div>
-          </div>
+
+          {!isLoading ? (
+            <>
+              <div className="pz-footer__col-1">
+                <ContactDetails {...contact_details} />
+              </div>
+              <div className="pz-footer__col-2">
+                <Brand />
+                <div className="pz-footer-meta">
+                  <span>
+                    <a href="https://pawelztef.me">Pawelztef</a>© 2020
+                  </span>
+                  <span>
+                    All rights reserved
+                  </span>
+                </div>
+                <SocialList />
+              </div>
+              <div className="pz-footer__col-3">
+                <div className="pz-footer__legal">
+                {menu.pages.map(entry => (
+                  <span className="pz-footer__legal-item">
+                    <Link to={entry.slug}>{entry.title}</Link>
+                  </span>
+                ))}
+                </div>
+              </div>
+            </>
+          ):(
+            <h3>loading...</h3>
+          )}
+
         </footer>
       </div>
     )
