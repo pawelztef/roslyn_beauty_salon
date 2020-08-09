ea1003be495e1012e7f1a1aa99866a85829990fa
Pavel <pawelztef@gmail.com>
2020-08-09 11:06:11 +0100

    NavigationBar component update to state.

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -10,7 +10,7 @@ class NavigationBar extends React.Component {
     super(props)
     this.state = {
       menu: {},
-      brand: {},
+      logo: {},
       callToAction: {},
       contactDetails: {},
       socialMedia: {},
@@ -21,10 +21,10 @@ class NavigationBar extends React.Component {
   async componentDidMount() {
     const response = await Axios.get(process.env.REACT_APP_DOMAIN + '/navbar')
     const data = response.data[0]
-    const { brand, call_to_action, contact_details, menu, social_media } = data
+    const { logo, call_to_action, contact_details, menu, social_media } = data
     this.setState({
       menu,
-      brand,
+      logo,
       callToAction: call_to_action,
       contactDetails: contact_details,
       socialMedia: social_media,
@@ -35,7 +35,7 @@ class NavigationBar extends React.Component {
 
 
   render() {
-    const { menu, brand, callToAction, contactDetails, isLoading } = this.state
+    const { menu, logo, callToAction, contactDetails, isLoading } = this.state
     return(
       <header className="pz-nav">
         {!isLoading && 
@@ -43,7 +43,11 @@ class NavigationBar extends React.Component {
             <TopNav contactDetails={this.state.contactDetails}
                     socialMedia={this.state.socialMedia}
             /> 
-            <BottomNav mobileMenuToggleHandler={this.props.mobileMenuToggleHandler} /> 
+            <BottomNav mobileMenuToggleHandler={this.props.mobileMenuToggleHandler}
+                       logo={this.state.logo}
+                       menu={this.state.menu}
+                       callToAction={this.state.callToAction}
+            /> 
           </>
         }
       </header>
