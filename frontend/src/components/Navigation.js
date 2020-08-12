fcae5b2af463d62f2bea3d50d164df2cc0881dd9
Pavel <pawelztef@gmail.com>
2020-08-12 13:59:24 +0100

    Quickfix on navigation imports.

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Axios from 'axios'
-import NavigationBar from './components/NavigationBar'
-import MobileNav from './components/MobileNav'
+import NavigationBar from './NavigationBar'
+import MobileNav from './MobileNav'
 
 class Navigation extends React.Component {
 
