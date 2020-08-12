8ac08463a0252f27687d3422bde1a0f8d3ff9d05
Pavel <pawelztef@gmail.com>
2020-08-12 13:27:30 +0100

    Necessary imports to main nav component.

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import Axios from 'axios'
+import NavigationBar from './components/NavigationBar'
+import MobileNav from './components/MobileNav'
 
 class Navigation extends React.Component {
 
