2ea51a3a13cf6a614dd0011d1649e5f40ac2302e
Pavel <pawelztef@gmail.com>
2020-08-17 16:33:42 +0100

    Connecting slider to data.

--- frontend/src/components/Slider.js
+++ frontend/src/components/Slider.js
@@ -1,20 +1,27 @@
 import React from 'react'
 
 const Slider = (props) =>  {
-  const {data} = props
-  const item = data.item[0]
-  const {image} = item
+  const hero_items = props.hero_item
   return (
-   <div className="pz-animated-banner">
-      <img className="pz-animated-banner__item" 
-           src={image.url} 
-           alt={image.alternativeText}/>
-      <div className="pz-animated-banner__item-top-layer">
-        <div className="pz-animated-banner__item-title pz-script-font">{item.title}</div>
-        <div className="pz-animated-banner__item-subtitle">{item.subtitle}</div>
-        <p className="pz-btn">View Our Services</p>
-      </div>
-   </div>
+    <> 
+      {hero_items &&
+        hero_items.map((entry) => {
+          const {id, title, subtitle, call_to_action, image } = entry
+          return(
+            <div key={id} className="pz-animated-banner">
+                  <img className="pz-animated-banner__item" 
+                    src={process.env.REACT_APP_DOMAIN+image.url} 
+                       alt={image.alternativeText}/>
+                  <div className="pz-animated-banner__item-top-layer">
+                    <div className="pz-animated-banner__item-title pz-script-font">{title}</div>
+                    <div className="pz-animated-banner__item-subtitle">{subtitle}</div>
+                    <a className="pz-btn" href={call_to_action.url}>{call_to_action.phrase}</a>
+                  </div>
+             </div>
+          )
+        })
+      }
+    </>
   )
 }
 
