5a1375f13a4ed99c50bbbb7b708920ebeeb3c90b
Pavel <pawelztef@gmail.com>
2020-08-18 11:01:58 +0100

    Connecting services to api.

--- frontend/src/components/Services.js
+++ frontend/src/components/Services.js
@@ -1,25 +1,68 @@
 import React from 'react'
 import EmptySpace from './EmptySpace'
-import ServicesList from './ServicesList'
+import { TiHeart } from "react-icons/ti"
+import Axios from 'axios'
 
-const services = props => {
-  console.log('services props ', props)
-  return(
-    <div className="pz-services__wrapper">
-      <EmptySpace />
-      <div className="pz-container-wrapper">
-        <div className="pz-services__container">
-          <h2 className="pz-section__header">Choose the service you need</h2>
-        </div>
-        <ServicesList />
+class Services extends React.Component {
+  state = {
+    treatments_categories: {},
+    isLoading: true
+  }
+
+  async componentDidMount() {
+    const response = await Axios.get(process.env.REACT_APP_DOMAIN+'/treatments-categories') 
+    const fetchedCategories = response.data
+    const passedCategories = this.props.treatments_categories
+    const newData = fetchedCategories.filter( category => passedCategories.filter( passedCategory => passedCategory.name === category.name ) )
+    this.setState({treatments_categories: newData, isLoading: false})
+  }
+
+
+  render() {
+    const { title, call_to_action} = this.props
+    let treatments_categories
+    if(!this.state.isLoading) {
+      treatments_categories = this.state.treatments_categories
+    }
+
+    return(
+      <div className="pz-services__wrapper">
         <EmptySpace />
-        <div className="pz-section__content">
-          <button className="pz-btn">View all Services</button>
+        <div className="pz-container-wrapper">
+          <div className="pz-services__container">
+            <h2 className="pz-section__header">{title}</h2>
+          </div>
+          <div className="pz-services">
+            {!this.state.isLoading &&
+              treatments_categories.map( (category) => {
+                return(
+                  <div className="pz-services__list">
+                    <h4 className="pz-services-header">{category.name}
+                      <span><TiHeart /></span>
+                    </h4>
+                    <ul className="pz-services-prices">
+                      {
+                        category.treatments.map((treatment) => {
+                          return(
+                            <li><a href="/services">{treatment.name}</a><br/><span>{treatment.price}€</span></li>
+                          )
+                        })
+                      }
+                    </ul>
+                  </div>
+                )
+              })
+            }
+          </div>
+          <EmptySpace />
+          <div className="pz-section__content">
+            <a href={call_to_action.url} className="pz-btn">{call_to_action.phrase}</a>
+          </div>
+          <EmptySpace />
         </div>
-        <EmptySpace />
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-export default services
+export default Services
