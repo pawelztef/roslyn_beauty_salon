fd1ff1731f0cdc066fe72e1d9b23a764a76cb096
Pavel <pawelztef@gmail.com>
2020-08-24 00:00:00 +0100

    Removing old backend.

--- frontend/src/components/AppointmentButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-
-export default function AppointmentButton(props) {
-  return(
-  <Link id='nav-btn' className="pz-btn pz-nav__appointment-button pz-invisible-lg"
-        to={props.url}>{props.phrase}</Link>
-  )
-}
