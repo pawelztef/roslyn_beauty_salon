e1c8589622c92a145361f7adf97958cd207e2ca3
Pavel <pawelztef@gmail.com>
2020-08-23 08:58:15 +0100

    Empty Space added to contact info with map.

--- frontend/src/components/ContactInfoWithMap.js
+++ frontend/src/components/ContactInfoWithMap.js
@@ -2,11 +2,13 @@ import React from 'react'
 import styled from 'styled-components'
 import ContactDetails from './ContactDetails'
 import GoogleMap from './GoogleMap'
+import EmptySpace from './EmptySpace'
 
 const ContactInfoWithMap = (props) => {
 
   return(
     <div className="pz-container-wrapper">
+      <EmptySpace />
       <div className="contact-info__map">
         <div className='contact-info__content'>
           <h4>Contact Info</h4>
@@ -14,6 +16,7 @@ const ContactInfoWithMap = (props) => {
         </div>
         <GoogleMap {...props.map_service} />
       </div>
+      <EmptySpace />
     </div>  
   )
 }
