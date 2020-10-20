f0bd38d16c3c510caf185ed86a87feb8b9c8979a
Pavel <pawelztef@gmail.com>
2020-10-21 00:00:00 +0100

    Files fixed. ScrollToTop files fixed - missing file suffix.

--- frontend/src/components/ScrollToTop.js
+++ frontend/src/components/ScrollToTop.js
@@ -2,17 +2,17 @@ import React from "react"
 import { withRouter } from "react-router-dom"
 
 class ScrollToTop extends React.Component {
-
   componentDidUpdate(prevProps) {
-    if(this.props.location.pathname !== prevProps.location.pathname ) {
+    if (
+      this.props.location.pathname !== prevProps.location.pathname
+    ) {
       window.scrollTo(0, 0)
     }
   }
 
   render() {
-    return null
+    return this.props.children
   }
-
 }
 
 export default withRouter(ScrollToTop)
